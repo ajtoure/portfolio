7559bcec44222e180d9fb8774d95c07c3e021e3b
ajtoure <assiyahjamilla@gmail.com>
2021-04-28 13:37:38 -0400

    further styling and layout changes

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-const portfolio = {};
+//The portfolio app will live here
+//the initial page slide will be animated
+//the gif will pause and pop up info when rolled on
+//this will apply to all the projects and their respective info
+//if clicked on, a pop up will ask if the user wishes to go to the project page
